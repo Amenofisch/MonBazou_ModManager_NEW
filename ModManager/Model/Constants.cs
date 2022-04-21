0e9cfa7cde2ec9589b9f39270a191486f558d50e
BossDarkReaper <johnp888@hotmail.co.uk>
2022-04-21 14:36:25 +0100

    Change: Remove unused directives

--- ModManager/Model/Constants.cs
+++ ModManager/Model/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MonBazou_ModManager.Model
+﻿namespace MonBazou_ModManager.Model
 {
     internal class Constants
     {
